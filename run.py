0262706c56d0e6588df9e254a7ad7e77fb3082cd
PressureRX <mu1ti@tistory.com>
2020-08-10 19:43:26 +0900

    로그파일 안뒤집어쓰게 변경

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ async def on_ready():
     ClanUnionDriver.ChannelInit()
 
     Logger.setLevel(logging.INFO)
-    LoggHandler = logging.FileHandler('actions.log', 'a', 'utf-8-sig')
+    LoggHandler = logging.FileHandler('actions.log', 'w', 'utf-8-sig')
     Logger.addHandler(LoggHandler)
 
     print("[*] Start ClanUnionBot")
